ac4a9d6dab89463f60a67dde675870cc44f9dd16
JAEHO SONG <thruthesky@gmail.com>
2020-11-19 02:16:44 +0900

    update for token test

--- tests/user.token.js
+++ tests/user.token.js
@@ -8,29 +8,29 @@ describe("User Push Notification Token", () => {
   it("users/{uid}/meta/tokens Add token on other's account should failed", async () => {
     const db = await setup(myAuth);
     const tokenDoc = db
-      .collection("users")
-      .doc(otherUid)
       .collection("meta")
-      .doc("tokens");
+      .doc("user")
+      .collection("token")
+      .doc(otherUid);
     await firebase.assertFails(tokenDoc.set({ [tokenId]: true }));
   });
 
   it("Add token should success", async () => {
     const db = await setup(myAuth);
     const tokenDoc = db
-      .collection("users")
-      .doc(myUid)
       .collection("meta")
-      .doc("tokens");
+      .doc("user")
+      .collection("token")
+      .doc(myUid);
     await firebase.assertSucceeds(tokenDoc.set({ [tokenId]: true }));
   });
   it("Read token should success", async () => {
     const db = await setup();
     const tokenDoc = db
-      .collection("users")
-      .doc(otherUid)
       .collection("meta")
-      .doc("tokens");
+      .doc("user")
+      .collection("token")
+      .doc(otherUid);
     await firebase.assertSucceeds(tokenDoc.get());
   });
 });
